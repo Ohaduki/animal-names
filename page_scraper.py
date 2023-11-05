461ca04c455a56b937c9e7bf712a888b1fd70563
Ohad <ohad.licht@gmail.com>
2023-11-05 19:53:55 +0200

    add function to get adjectives and return list of adjectives with the animals

--- page_scraper.py
+++ page_scraper.py
@@ -6,3 +6,23 @@ def get_page(url):
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup
 
+def get_adjectives():
+    adjectives = {}
+    soup = get_page('https://en.wikipedia.org/wiki/List_of_animal_names')
+    wiki_tables = soup.find_all('table', class_='wikitable')
+    rows = wiki_tables[1].find_all('tr')
+    for row in rows[1:]:
+        cells = row.find_all('td')
+        if len(cells) > 1:
+            if len(cells[5].text) > 1:
+                if cells[5].text not in adjectives.keys():
+                    adjectives[cells[5].text] = [cells[0].text]
+                else:
+                    adjectives[cells[5].text].append(cells[0].text)
+    return adjectives
+
+print(get_adjectives())
+            
+
+
+get_adjectives()
